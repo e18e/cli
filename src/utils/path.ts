71601360bc0d9d34f6daccf49404cd1034de2d35
outslept <135520429+outslept@users.noreply.github.com>
2025-08-04 02:03:26 +0300

    fix: windows path separators

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,11 +1,3 @@
-export const isWindows = typeof process !== 'undefined' && process.platform === 'win32';
-
-const backslashRegex = /\\/g;
-
-function slash(path: string): string {
-  return path.replace(backslashRegex, '/');
-}
-
-export function normalizePath(id: string): string {
-  return require('node:path').posix.normalize(isWindows ? slash(id) : id);
+export function normalizePath(path: string): string {
+  return path.replaceAll('\\', '/');
 }
